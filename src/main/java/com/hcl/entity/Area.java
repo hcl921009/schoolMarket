cf290308774880cde84feec567a231f2ce60dd3c
Cashmore001 <Cashmore001@users.noreply.github.com>
2018-07-02 11:37:53 +0800

    First update Framework created

---
+++ src/main/java/com/hcl/entity/Area.java
@@ -0,0 +1,52 @@
+package com.hcl.entity;
+
+
+import java.util.Date;
+
+public class Area {
+    private Integer areaId;//ID
+    private String areaName;//区域名称
+    private Integer priority;//区域优先级
+    private Date creatTime;//创建时间
+    private Date lastTime;//更新时间
+
+    public Integer getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Integer areaId) {
+        this.areaId = areaId;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreatTime() {
+        return creatTime;
+    }
+
+    public void setCreatTime(Date creatTime) {
+        this.creatTime = creatTime;
+    }
+
+    public Date getLastTime() {
+        return lastTime;
+    }
+
+    public void setLastTime(Date lastTime) {
+        this.lastTime = lastTime;
+    }
+}
