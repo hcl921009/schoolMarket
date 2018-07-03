a1783c196f82020359a532850b828c62d5e83f9c
Cashmore001 <Cashmore001@users.noreply.github.com>
2018-07-04 00:46:53 +0800

    Debug: jdbc.url=...&serverTimezone=GMT%2B8 is necessary

---
+++ src/test/java/com/hcl/BaseTest.java
@@ -0,0 +1,14 @@
+package com.hcl;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/*
+    配置Spring与Junit整合，Junit启东市加载springIOC容器
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉Junit,Spring的配置文件的位置
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class BaseTest {
+}
---
+++ src/test/java/com/hcl/dao/AreaDaoTest.java
@@ -0,0 +1,25 @@
+package com.hcl.dao;
+
+
+
+import com.hcl.BaseTest;
+import com.hcl.entity.Area;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class AreaDaoTest extends BaseTest {
+    @Autowired
+    private AreaDao areaDao;
+
+    @Test
+    public void testQueryArea(){
+        List<Area> areaList = areaDao.queryArea();
+        assertEquals(2,areaList.size());
+    }
+
+
+}
